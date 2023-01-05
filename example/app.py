240a6d625581aaa8b979d4607c59294380d05f78
Michael Combs <michael.combs@pangea.cloud>
2023-01-05 13:12:16 -0800

    Initial Commit

---
+++ example/app.py
@@ -0,0 +1,23 @@
+'''
+This module randomly generates lorem ipsum text with ssn numbers included
+'''
+import random
+
+from lorem.text import TextLorem
+from flask import Flask
+from lorem.data import WORDS
+from faker import Faker
+
+
+fake = Faker()
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def random_ssn():
+    # Generate a random number of lorem
+    words = WORDS + [fake.ssn() for _ in range(random.randint(0, 11))]
+    text_generator = TextLorem(words=words)
+    return text_generator.text()
