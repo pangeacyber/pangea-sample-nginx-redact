86a9c632d132f9cccceb9b2f1613325e8254c0ad
Michael Combs <michael.combs@pangea.cloud>
2023-01-06 12:54:36 -0800

    Add email PII to generator

--- example/app.py
+++ example/app.py
@@ -18,6 +18,6 @@ app = Flask(__name__)
 @app.route("/")
 def random_ssn():
     # Generate a random number of lorem
-    words = WORDS + [fake.ssn() for _ in range(random.randint(0, 11))]
+    words = WORDS + [fake.ssn() for _ in range(random.randint(0, 11))] + [fake.email() for _ in range(random.randint(0, 11))]
     text_generator = TextLorem(words=words)
     return { "text": text_generator.text() }
